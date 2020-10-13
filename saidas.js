9406b596e4cee845890c6a168bac857b4e4bdbda
Jordan Bryan <jbryan@MacBook-de-Leonardo.local>
2020-10-13 16:27:24 -0300

    Print File

---
+++ saidas.js
@@ -0,0 +1,9 @@
+var nome = prompt("Digite o seu nome:")
+var idade = prompt("Digite a sua idade:")
+var profissao = prompt("Digite a sua profissão:")
+
+console.log()
+console.log("--------------")
+console.log(nome)
+console.log(idade)
+console.log(profissao)
